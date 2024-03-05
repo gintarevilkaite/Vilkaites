3cd105f2481743986dab6bc98ba2321a8150c03d
gintarevilkaite <gintare.vilkaite2000@gmail.com>
2024-03-05 21:42:33 +0200

    Added change text functionality, change color button and functionality

--- app/src/main/java/com/example/vilkaites/MainActivity.java
+++ app/src/main/java/com/example/vilkaites/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.vilkaites;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void btnChangeTextClick(View view) {
+        this.tvMain.setText("Hello");
+    }
+
+    public void btnChangeColor(View view) {
+        this.tvMain.setTextColor(R.color.Red);
     }
 }
